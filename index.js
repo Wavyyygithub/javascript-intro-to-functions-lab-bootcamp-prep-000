3e9cd6178b58339a33df6e628080b97459d8dda7
Corry Seay <corry_seay@yahoo.com>
2020-07-09 22:38:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return "I can't hear you!"
+}
+
+function sayHiToGrandma(string) {
   return "YES INDEED!"
-  return "I love you, too."
 }
