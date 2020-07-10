62a9df9a7b91bbe00513e75bd0eba014574df78a
Corry Seay <corry_seay@yahoo.com>
2020-07-10 01:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ mixedCase.toLowerCase() === mixedCase // false
  
 mixedCase.toUpperCase() === mixedCase // false
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
