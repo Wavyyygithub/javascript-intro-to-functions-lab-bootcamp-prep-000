567d988076d933a81274c8e85f78386522d222fb
Corry Seay <corry_seay@yahoo.com>
2020-07-10 00:25:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ console.log (string.toUpperCase())
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
-
-function sayHiToGrandma(string) {
-return "I can't hear you!"
-}
+ function sayHiToGrandma(string) {
+   return "I can't hear you!"
+ }
